aed0c6c476b1a662bcf074541f27dfc4dc4c6900
Dennis Müller <de_br@uni-bremen.de>
2024-07-11 11:19:38 -0700

    Add basic test to satisfy CI/CD pipeline

---
+++ tests/ports_test.py
@@ -0,0 +1,19 @@
+"""
+TODO: Expand. For now only one test to satisfy CI/CD pipeline.
+"""
+
+import numpy as np
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+from livenodes_common_ports.ports.compounds import Port_Timeseries
+
+
+class TestProcessing:
+
+    def test_port_timeseries_valid(self):
+        port_ts = Port_Timeseries()
+        result, msg = port_ts.check_value(np.arange(100).reshape((20, 5)))
+        assert result
+        assert msg is None
